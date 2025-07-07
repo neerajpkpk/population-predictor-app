77ba052d90ff98760319ff3ec0ac22e933fafcdb
neeraj gupta <neerajgutagupta@gmail.com>
2025-07-07 15:19:51 +0530

    Configured PostgreSQL for Render deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,16 +85,29 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'demo',              # database name
+#         'USER': 'postgres',          # default PostgreSQL user
+#         'PASSWORD': '314818#Nee', # ← apna postgres password daalo
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'demo',              # database name
-        'USER': 'postgres',          # default PostgreSQL user
-        'PASSWORD': '314818#Nee', # ← apna postgres password daalo
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
 }
+
 
 
 
